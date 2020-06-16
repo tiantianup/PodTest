6b073577524414d6d4e1276d79076ce290d9ddfb
tiantianup <wyztiantianup@163.com>
2020-06-16 15:35:25 +0800

    修改文件

---
+++ Base/ZMNViewController.h
@@ -0,0 +1,17 @@
+//
+//  ZMNViewController.h
+//  PXViewOpInspect
+//
+//  Created by qcsd02 on 2020/6/16.
+//  Copyright © 2020 习波 肖. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+NS_ASSUME_NONNULL_BEGIN
+
+@interface ZMNViewController : UIViewController
+
+@end
+
+NS_ASSUME_NONNULL_END
---
+++ Base/ZMNViewController.m
@@ -0,0 +1,32 @@
+//
+//  ZMNViewController.m
+//  PXViewOpInspect
+//
+//  Created by qcsd02 on 2020/6/16.
+//  Copyright © 2020 习波 肖. All rights reserved.
+//
+
+#import "ZMNViewController.h"
+
+@interface ZMNViewController ()
+
+@end
+
+@implementation ZMNViewController
+
+- (void)viewDidLoad {
+    [super viewDidLoad];
+    // Do any additional setup after loading the view.
+}
+
+/*
+#pragma mark - Navigation
+
+// In a storyboard-based application, you will often want to do a little preparation before navigation
+- (void)prepareForSegue:(UIStoryboardSegue *)segue sender:(id)sender {
+    // Get the new view controller using [segue destinationViewController].
+    // Pass the selected object to the new view controller.
+}
+*/
+
+@end
